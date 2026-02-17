3429f42481a7e1a083b630c94e595a6fac24636d
Nicu Siderias <nicu.siderias@analog.com>
2026-02-17 14:13:10 +0200

    Add necessary files to create debian packages
    Signed-off-by: Nicu Siderias <nicu.siderias@analog.com>

---
+++ .github/scripts/create_debian.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+version=$1
+architecture=$(dpkg --print-architecture)
+source_code=$(basename "$PWD")
+
+# Install dependencies based on user (root or non-root)
+if [ "$(id -u)" -eq 0 ]; then
+    apt-get update
+    apt-get install -y build-essential cmake pkg-config libgtk-3-dev libncurses-dev devscripts debhelper libiio-dev 
+else
+    sudo apt-get update
+    sudo apt-get install -y build-essential cmake pkg-config libgtk-3-dev libncurses-dev devscripts debhelper libiio-dev
+fi
+
+# Update version and architecture in debian files
+sed -i "s/@VERSION@/$version-1/" packaging/debian/changelog
+sed -i "s/@DATE@/$(date -R)/" packaging/debian/changelog
+sed -i "s/@ARCHITECTURE@/$architecture/" packaging/debian/control
+
+# Copy debian directory to source root
+cp -r packaging/debian .
+
+# Remove packaging directory to avoid including it in the source tarball
+rm -rf packaging
+
+# Create the orig tarball
+pushd ..
+tar czf ${source_code}_${version}.orig.tar.gz \
+    --exclude='.git' \
+    --exclude='debian' \
+    $source_code
+popd
+
+# Build the debian package
+debuild
